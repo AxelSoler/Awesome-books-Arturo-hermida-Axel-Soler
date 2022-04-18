b389e965baed928d79da1dcaac05bfe0bafcf135
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-18 17:45:30 -0300

    fix linter errors

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const div = document.querySelector('.books'); 
+const div = document.querySelector('.books');
 
 let books = [];
 
-const deleteItem = (i)=>{
+const deleteItem = (i) => {
   books.splice(i, 1);
   localStorage.setItem('listBooks', JSON.stringify(books));
-}
+};
 
 const display = () => {
   div.innerHTML = '';
-  for ( let i = 0 ; i < books.length; i += 1){
+  for (let i = 0; i < books.length; i += 1) {
     const divList = document.createElement('div');
     const name = document.createElement('h3');
     name.innerHTML = books[i].title;
@@ -18,26 +18,26 @@ const display = () => {
     const remBtn = document.createElement('button');
     remBtn.innerHTML = 'Remove';
 
-    remBtn.addEventListener('click', () =>{
+    remBtn.addEventListener('click', () => {
       deleteItem(i);
       display();
     });
-    
+
     divList.appendChild(name);
     divList.appendChild(aut);
     divList.appendChild(remBtn);
 
     div.appendChild(divList);
-    }
+  }
 };
 
 const addBook = (ev) => {
   ev.preventDefault();
 
-  let book = {
+  const book = {
     title: document.querySelector('.inputTitle').value,
     author: document.querySelector('.inputAuthor').value,
-  }
+  };
 
   books.push(book);
   document.querySelector('.addBook').reset();
@@ -45,16 +45,15 @@ const addBook = (ev) => {
   display();
 
   localStorage.setItem('listBooks', JSON.stringify(books));
-  
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('addBtn').addEventListener('click', addBook)
+  document.getElementById('addBtn').addEventListener('click', addBook);
 });
 
-window.addEventListener('load', () =>{
-  if(localStorage.getItem('listBooks')){
+window.addEventListener('load', () => {
+  if (localStorage.getItem('listBooks')) {
     books = JSON.parse(localStorage.getItem('listBooks'));
     display();
   }
-})
+});
